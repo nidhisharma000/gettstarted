daf04cd1f9ecc898c287d15955c1717ed3ae2484
nidhisharma000 <nidhisharma000@gmail.com>
2019-09-16 00:11:17 +1000

    update requirements.txt trails 10

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = "!^_6%0so9$a@u-w22nc56xcp0^spoo4k^3q!j016o5hll+#c#o"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'gettstarted.herokuapp.com']
 
 
 # Application definition
